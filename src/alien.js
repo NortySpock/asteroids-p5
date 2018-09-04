302321711b3f2b40362fd19470b65a8cdf4b3ae0
David Norton <david.k.norton@gmail.com>
2018-09-03 19:47:52 -0500

    some hacks to settle down after getting the alien tracking to work

--- src/alien.js
+++ src/alien.js
@@ -16,18 +16,12 @@ class Alien
         this.scl = min(canvasHeight,canvasWidth) / 28;
         this.collideRadius = this.scl * 2;
         this.deleteFlag = false;
-        this.accelRate = 1;
-        this.maxSpeed = 5;
+        this.accelRate = 0.75;
+        this.maxSpeed = 2;
         this.targetHeadingRadians = 0;
         this.targetPoint;
         this.patrolPointNW = createVector(canvasWidth*(1/4),canvasHeight*(1/4));
-        this.patrolPointN = createVector(canvasWidth*(2/4),canvasHeight*(1/4));
-        this.patrolPointNE = createVector(canvasWidth*(3/4),canvasHeight*(1/4));
-        
-        
-        this.patrolPointSW = createVector(canvasWidth*(1/4),canvasHeight*(3/4));
-        this.patrolPointS = createVector(canvasWidth*(2/4),canvasHeight*(3/4));        
-        this.patrolPointSE = createVector(canvasWidth*(3/4),canvasHeight*(3/4));
+        this.patrolPointN = createVector(canvasWidth*(2/4),canvasHeight*(1/4));        
     }
 
     render()
@@ -41,52 +35,20 @@ class Alien
       line(this.pos.x-subScl,this.pos.y-subScl, this.pos.x-subScl,this.pos.y+subScl)
       line(this.pos.x+subScl,this.pos.y-subScl, this.pos.x+subScl,this.pos.y+subScl)
       point(this.pos.x, this.pos.y);
-      
-      /* indicate patrol points */
-      point(this.patrolPointNW.x, this.patrolPointNW.y);
-      point(this.patrolPointN.x, this.patrolPointN.y);
-      point(this.patrolPointNE.x, this.patrolPointNE.y);
-      
-      point(this.patrolPointSW.x, this.patrolPointSW.y);
-      point(this.patrolPointS.x, this.patrolPointS.y);
-      point(this.patrolPointSE.x, this.patrolPointSE.y);
-      
-      /* indicate target heading */
-      if(this.targetPoint)
-      {
-        stroke(255,0,0);
-        line(this.pos.x,this.pos.y,this.targetPoint.x,this.targetPoint.y)
-      }
       pop();      
     }
 
     update()
     {
       //determine heading
-      var targetPoint = this.patrolPointSW;
+      var targetPoint = createVector(50,50);
       this.targetPoint = targetPoint;
       this.graviticPull(targetPoint);
 
       this.vel = this.vel.limit(this.maxSpeed); //speed limiter
       this.pos.add(this.vel);
 
-      //appear on other edge if we go offscreen
-      if(this.pos.x > canvasWidth)
-      {
-        this.pos.x = 0;
-      }
-      if(this.pos.x < 0)
-      {
-        this.pos.x = canvasWidth;
-      }
-      if(this.pos.y > canvasHeight)
-      {
-        this.pos.y = 0;
-      }
-      if(this.pos.y < 0)
-      {
-        this.pos.y = canvasHeight;
-      }
+      this.handleGoingOffscreen();
     }
 
     checkCollision(x,y,radius)
@@ -120,10 +82,10 @@ class Alien
 
     graviticPull(targetPoint)
     {
-      var targetAccelVector = createVector(0,0);
-      targetAccelVector.sub(targetPoint)
-      //targetAccelVector.normalize();
-      this.vel.sub(targetAccelVector);
+      var targetAccelVector = p5.Vector.sub(targetPoint,this.pos)      
+      targetAccelVector.normalize();
+      targetAccelVector.mult(this.accelRate);
+      this.vel.add(targetAccelVector);
     }
 
     hit()
@@ -146,5 +108,26 @@ class Alien
 
       }
       return angle;
+    }
+    
+    handleGoingOffscreen()
+    {
+      //appear on other edge if we go offscreen
+      if(this.pos.x > canvasWidth)
+      {
+        this.pos.x = 0;
+      }
+      if(this.pos.x < 0)
+      {
+        this.pos.x = canvasWidth;
+      }
+      if(this.pos.y > canvasHeight)
+      {
+        this.pos.y = 0;
+      }
+      if(this.pos.y < 0)
+      {
+        this.pos.y = canvasHeight;
+      }
     }
 }
